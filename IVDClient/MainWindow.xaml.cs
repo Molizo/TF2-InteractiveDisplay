b8b332ad3822c242566257679f5526fab8804852
Mihnea Theodor Vișoiu <visoiu.mihnea@gmail.com>
2022-06-01 21:37:27 +0300

    Refactored code to require less FS watchers and added tabbing on the main window

--- IVDClient/MainWindow.xaml.cs
+++ IVDClient/MainWindow.xaml.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Reflection;
 using System.Text;
 using System.Text.Json;
